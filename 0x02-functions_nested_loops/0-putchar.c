f06b31bfb105a66312660d2c6a4df1054fc8317b
Notford <kwame.notford@gmail.com>
2023-02-21 01:35:48 -0800

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,16 @@
  * Return: int
  */
 
-int main(void) {
+int main(void)
+{
 	int n = 0;
 	char str_ptr[8] = "_putchar";
 
-	while (n < 8) {
+	while (n < 8)
+	{
 		_putchar(str_ptr[n]);
 		n += 1;
 	}
 	_putchar('\n');
 	return (0);
-
 }
