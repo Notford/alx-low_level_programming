4935212269b172dbd483f9ce36fd4b0f45b8549f
Notford <kwame.notford@gmail.com>
2023-02-21 05:12:22 -0800

    fix betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,19 @@
 
 void print_alphabet_x10(void)
 {
-        int l;
-        int n = 0;
+	int l;
+	int n = 0;
 
-        while (n < 10)
-        {
-                l = 'a';
-                while (l <= 'z')
-                {
-                        _putchar(l);
-                        l += 1;
-                }
-                _putchar(10);
-                n += 1;
-        }
+	while (n < 10)
+	{
+		l = 'a';
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l += 1;
+		}
+		_putchar(10);
+		n += 1;
+	}
+	return (0);
 }
-
