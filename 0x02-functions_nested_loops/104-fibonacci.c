a8a74a6f14b700e5d3da3fbcc387860d7cbc23a4
Notford <kwame.notford@gmail.com>
2023-02-21 11:57:35 -0800

    Task 14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, separated by a comma followed by a space.
- *@return : Always 0
+ *@return : 0
  */
 
 int main(void)
@@ -43,5 +43,5 @@ int main(void)
 	fib2_half2 = half2;
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
