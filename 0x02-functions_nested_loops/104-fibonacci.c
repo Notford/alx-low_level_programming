6dbc9fe3dcfcd6b5cc6874be620b20b879f507f7
Notford <kwame.notford@gmail.com>
2023-02-21 11:46:20 -0800

    Task 14

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,10 +3,11 @@
 /**
  * main - Prints the first 98 Fibonacci numbers, starting with
  * 1 and 2, separated by a comma followed by a space.
- * return : 0.
+ * return : Always 0
  */
 
 int main(void)
+
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
