0d4282472dfebd8906bc76047fb9c85f5036de9f
Notford <kwame.notford@gmail.com>
2023-02-23 05:45:22 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c); 
+int null(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(void);
+void print_diagonal(void);
+void print_square(void);
+void print_triangle(void);
+
+#endif
